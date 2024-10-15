130a309bcc6e6f56c4e535d72eb9a65dc182bea8
asg475 <you@example.comasg475@alumnos.unican.es>
2024-10-15 10:01:50 +0200

    Funcion pay_raise creado, aumenta un 15% el sueldo

--- '''Obejetos Empleado'''.py
+++ '''Obejetos Empleado'''.py
@@ -1,6 +1,7 @@
 '''Obejetos  Empleado'''
 import random
 
+
 class Empleado:
     id_previa = 1
 
@@ -13,10 +14,20 @@ class Empleado:
         self.pay = random.randint(1500, 2200)
 
         Empleado.id_previa +=  1
-        
+    
+
+    def pay_raise(self):
+        self.pay = self.pay * 1.15
+        return print("aumento realizado")
+
+
 sergio = Empleado("sergio", "pesquera")
 print(sergio.id)
 print(sergio.name)
 print(sergio.surname)
 print(sergio.email)
+print(sergio.pay)
+miguel = Empleado("miguel", "Betegon")
+print(miguel.id)
+sergio.pay_raise()
 print(sergio.pay)
