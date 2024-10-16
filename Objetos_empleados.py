3c47e9f5543bfa076501fdb50bd24c4b658cb510
asg475 <you@example.comasg475@alumnos.unican.es>
2024-10-16 09:04:04 +0200

    ahora si se supone que se ha cambiado el nombre al archivo

--- Objetos_empleados.py
+++ Objetos_empleados.py
@@ -10,23 +10,52 @@ class Empleado:
         self.id = Empleado.id_previa
         self.name = name
         self.surname = surname
-        self.email = name + "." + surname + "@email.com"
+        self.email = f"{name}.{surname}@email.com"
         self.pay = random.randint(1500, 2200)
 
         Empleado.id_previa += 1
 
     def pay_raise(self):
+        """ Esta funcion proporciona un aumento del 15%
+        a un elemento construido con la clase Empleado.
+
+        Returns:
+            El string: aumento realizado
+        """
         self.pay = int( self.pay * 1.15)
-        return print("aumento realizado")
+        return "aumento realizado"
 
     def __str__(self):
+        """Esta funcion devuelve un string al hacer un print
+        de un elemento creado con la clase Empleado
+
+        Returns:
+            string:con el nombre del empleado y su sueldo
+        """
         return f"El empleado {self.name} cobra {self.pay}"
     
     def __add__(self, otro):
+        """Suma el sueldo de dos empleados
+
+        Args:
+            otro (Empleado): el segundo empleado con el que se quiere sumar el sueldo
+
+        Returns:
+            string: Dice la cuanto combran ambos empleados juntos
+        """
         return f"Juntos combran {self.pay + otro.pay}"
     
     @classmethod
     def from_string(cls, cadena: str):
+        """permite crear un empleado cuando todos los datos se 
+        encuentran en un unico string
+
+        Args:
+            cadena (str): string que contien el nombre y el apellido separado por una coma
+
+        Returns:
+            Empleado: devuelve directamente un elemento con todos los atributos de Empleado
+        """
         name, surname = cadena.split(", ")
         return cls(name, surname)
 
@@ -38,11 +67,23 @@ class CEO(Empleado):
         self.empleados_a_cargo = []
     
     def add_employee(self, empleado):
+        """Añade a la lista de empleados_a_cargo
+        propia de cada CEO un nuevo empleado
+
+        Args:
+            empleado (Empleado)
+        """
         if isinstance(empleado, Empleado):
             self.empleados_a_cargo.append(empleado)
             print(f"{empleado.name} {empleado.surname} ha sido añadido a la lista de empleados a cargo.")
 
     def remove_employee(self, empleado):
+        """Elimina de la lista de empleados_a_cargo
+        propia de cada CEO  empleado
+
+        Args:
+            empleado (Empleado)
+        """
         if empleado in self.empleados_a_cargo:
             self.empleados_a_cargo.remove(empleado)
             print(f"{empleado.name} {empleado.surname} ha sido eliminado de la lista de empleados a cargo.")
@@ -50,6 +91,8 @@ class CEO(Empleado):
             print(f"{empleado.name} {empleado.surname} no está en la lista de empleados a cargo.")
 
     def listar_empleados(self):
+        """Devuelve por pantalla los empleado que tiene a su cargo cada CEO
+        """
         if not self.empleados_a_cargo:
             print("No hay empleados a cargo.")
         else:
